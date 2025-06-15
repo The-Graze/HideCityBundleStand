d309f1fbee6775ffff1264221342d2401fa85510
Golden <cute.golden.protogen.2@gmail.com>
2025-06-14 21:06:00 -0400

    Update Plugin.cs

--- HideCityBundleStand/Plugin.cs
+++ HideCityBundleStand/Plugin.cs
@@ -18,6 +18,7 @@ namespace HideCityBundleStand
         private void GStart()
         {
 			Transform ForestKiosk = GameObject.Find("LocalObjects_Prefab").transform.FindChildRecursive("ForestKiosk_Anchor");
+			Transform2 CrittersKiosk = GameObject.Find("Networking Scripts").transform.FindChildRecursive("BundleManager");
 			DisabledStuff = new()
 			{
                 ForestKiosk.transform.FindChildRecursive("gorilla_new").GetChild(2).gameObject,
@@ -26,6 +27,14 @@ namespace HideCityBundleStand
                 ForestKiosk.FindChildRecursive("PurchaseButton").gameObject,
                 ForestKiosk.FindChildRecursive("CreatorCodeMonitor").gameObject,
                 ForestKiosk.FindChildRecursive("FrontPanel_Center").gameObject,
+		ForestKiosk.FindChildRecursive("MedusaHair Wardrobe Variant").gameObject,
+		CrittersKiosk.transform.FindChildRecursive("gorilla_new").GetChild(2).gameObject,
+                CrittersKiosk.FindChildRecursive("EndCap_PackSign").gameObject,
+                CrittersKiosk.FindChildRecursive("EndCap_BackgroundTexture").gameObject,
+                CrittersKiosk.FindChildRecursive("PurchaseButton").gameObject,
+                CrittersKiosk.FindChildRecursive("CreatorCodeMonitor").gameObject,
+                CrittersKiosk.FindChildRecursive("FrontPanel_Center").gameObject,
+		CrittersKiosk.FindChildRecursive("MedusaHair Wardrobe Variant").gameObject,
             };
             foreach (var g in DisabledStuff)
             {
