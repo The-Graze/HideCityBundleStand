0fadbe59314350f06b2f993f94f74f7bf9cec766
Golden <cute.golden.protogen.2@gmail.com>
2025-06-30 21:11:56 -0400

    Update Plugin.cs

--- HideCityBundleStand/Plugin.cs
+++ HideCityBundleStand/Plugin.cs
@@ -17,24 +17,17 @@ namespace HideCityBundleStand
 
         private void GStart()
         {
-			Transform ForestKiosk = GameObject.Find("LocalObjects_Prefab").transform.FindChildRecursive("ForestKiosk_Anchor");
-			Transform2 CrittersKiosk = GameObject.Find("Networking Scripts").transform.FindChildRecursive("BundleManager");
-			DisabledStuff = new()
-			{
+	    Transform ForestKiosk = GameObject.Find("LocalObjects_Prefab").transform.FindChildRecursive("ForestKiosk_Anchor");
+	    DisabledStuff = new()
+	    {
                 ForestKiosk.transform.FindChildRecursive("gorilla_new").GetChild(2).gameObject,
                 ForestKiosk.FindChildRecursive("EndCap_PackSign").gameObject,
                 ForestKiosk.FindChildRecursive("EndCap_BackgroundTexture").gameObject,
                 ForestKiosk.FindChildRecursive("PurchaseButton").gameObject,
                 ForestKiosk.FindChildRecursive("CreatorCodeMonitor").gameObject,
                 ForestKiosk.FindChildRecursive("FrontPanel_Center").gameObject,
-		ForestKiosk.FindChildRecursive("MedusaHair Wardrobe Variant").gameObject,
-		CrittersKiosk.transform.FindChildRecursive("gorilla_new").GetChild(2).gameObject,
-                CrittersKiosk.FindChildRecursive("EndCap_PackSign").gameObject,
-                CrittersKiosk.FindChildRecursive("EndCap_BackgroundTexture").gameObject,
-                CrittersKiosk.FindChildRecursive("PurchaseButton").gameObject,
-                CrittersKiosk.FindChildRecursive("CreatorCodeMonitor").gameObject,
-                CrittersKiosk.FindChildRecursive("FrontPanel_Center").gameObject,
-		CrittersKiosk.FindChildRecursive("MedusaHair Wardrobe Variant").gameObject,
+		ForestKiosk.FindChildRecursive("rig").gameObject,
+                ForestKiosk.FindChildRecursive("PartyPayloadBackpack_Wardrobe Variant").gameObject
             };
             foreach (var g in DisabledStuff)
             {
